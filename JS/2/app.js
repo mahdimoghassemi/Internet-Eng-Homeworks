596f9b89ac91154c3b8905b8dc4e8d586db7d94c
Mahdi Moghassemi <mahdimoghassemi@gmail.com>
2023-05-10 22:51:50 +0330

    Done

--- JS/2/app.js
+++ JS/2/app.js
@@ -1,24 +1,46 @@
-let input_task, second_number, button;
-let first, second, result;
+let input, button, add;
+let input_text, count_num = 1;
 
 let init = function() {
-    input_task = document.querySelector('.inputTask');
     button = document.querySelector('.add-btn');
-    result = document.querySelector('.result');
+    input = document.querySelector('.inputTask');
+    add = document.querySelector('.add');
 
 }
 
 init();
 
-first_number.addEventListener('input' , function(e) {
-    first = Number(e.target.value);
-})
+// console.log(input);
+input.addEventListener('input', function(e) {
+    input_text = e.target.value;
+    // console.log(input_text);
+});
 
-second_number.addEventListener('input' , function(e) {
-    second = Number(e.target.value);
-})
+button.addEventListener('click', function(e) {
 
+    let head = document.createElement('tr');
+    add.appendChild(head);
 
-button.addEventListener('click' , function(e) {
-    result.textContent = first*second;
-})
+    let count = document.createElement('td');
+    count.innerHTML = count_num;
+    count_num += 1;
+    head.appendChild(count);
+
+    let task_name = document.createElement('td');
+    task_name.innerHTML = input_text;
+    head.appendChild(task_name);
+
+    let button_td = document.createElement('td');
+    head.appendChild(button_td);
+
+    let task_button = document.createElement('button');
+    task_button.className = "btn btn-outline-primary done_btn";
+    task_button.innerText = "Done";
+    button_td.appendChild(task_button);
+});
+
+let done_btn = document.querySelector('.done_btn');
+
+done_btn.addEventListener('click', function(e) {
+    e.target.parentNode.remove();
+});
